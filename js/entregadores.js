fafecc484805ebedc4fa932340d789cf60932502
vtAndreola <dev.andreola@gmail.com>
2024-09-17 15:20:12 -0300

    adicionar header interativo

--- js/entregadores.js
+++ js/entregadores.js
@@ -1,21 +1,16 @@
-import {nombre, placas, modelos} from './intermediario.js';
+import { nombre, placas, modelos } from "./intermediario.js";
 
+function atualizar() {
+  dados;
+  var tabela = document.getElementById("tabela");
 
-function atualizar(){
-    
-    dados
-    var tabela = document.getElementById('tabela')
-    
-    
-    
-        for(let i = 0; i < n; i++){
-            var n = i + 1;
-            let newI = document.createElement('tr')
-            tabela.appendChild(newI)
-            let newL = document.createElement('tr')
-            tabela.appendChild(newL)
-            newI.innerHTML = `<td id="nome">${nombre.value}<summary>entregas realizadas:297</summary></td>`
-            newL.innerHTML = `<td id="placa">${placas.value}<summary>${modelos.value}</summary></td>`}
-    
-    
-    }
+  for (let i = 0; i < n; i++) {
+    var n = i + 1;
+    let newI = document.createElement("tr");
+    tabela.appendChild(newI);
+    let newL = document.createElement("tr");
+    tabela.appendChild(newL);
+    newI.innerHTML = `<td id="nome">${nombre.value}<summary>entregas realizadas:297</summary></td>`;
+    newL.innerHTML = `<td id="placa">${placas.value}<summary>${modelos.value}</summary></td>`;
+  }
+}
