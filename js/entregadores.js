0549ba530a5f4383a767c591697390299cb481c4
Pedro-reis <pedrosouzadreis@gmail.com>
2024-09-17 22:56:55 -0300

    finalizado

--- js/entregadores.js
+++ js/entregadores.js
@@ -1,16 +1,20 @@
-import { nombre, placas, modelos } from "./intermediario.js";
+
 
 function atualizar() {
-  dados;
-  var tabela = document.getElementById("tabela");
+  let nome = ["Vitor Andreola","Lucas Bezzera","Pedro Souza","Lucas Simionato","Vitor Criminal"]
+  let placa = ["HBD-1124","FGK-0099","QWE-5566","ZXC-8866","DFG-1349","UIO-7766"]
+  let modelo = ["CG 150 Fan Flex","HONDA ADV","PCX","FORZA 350","CG 160 START"]
+  let tabela = document.getElementById("tabela");
+  const n = nome.keys()
+for (let i of n) {
+  let newI = document.createElement("tr");
+  tabela.appendChild(newI);
+  let newL = document.createElement("tr");
+  tabela.appendChild(newL);
+  
+  newI.innerHTML += `<td ><img src="img/capacete.png" alt="moto" /></td>
+                    <td id="nome">${nome[i]}<summary>entregas realizadas:297</summary></td>
+                    <td id="placa">${placa[i]}<summary>${modelo[i]}</summary></td>`;
+}
 
-  for (let i = 0; i < n; i++) {
-    var n = i + 1;
-    let newI = document.createElement("tr");
-    tabela.appendChild(newI);
-    let newL = document.createElement("tr");
-    tabela.appendChild(newL);
-    newI.innerHTML = `<td id="nome">${nombre.value}<summary>entregas realizadas:297</summary></td>`;
-    newL.innerHTML = `<td id="placa">${placas.value}<summary>${modelos.value}</summary></td>`;
-  }
 }
